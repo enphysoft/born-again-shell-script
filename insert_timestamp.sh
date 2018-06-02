85132ed4a23a012042de19578a9bd41a8483bc7e
enphysoft <albertsk@gmail.com>
2018-06-01 17:14:30 -1000

    Comments of insert_timestamp were added.

--- insert_timestamp.sh
+++ insert_timestamp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mydate=`date "+%Y-%m-%d-%H-%M-%S-%p"`
 echo $mydate | xargs echo -n | xclip -selection clipboard | xvkbd -xsendevent -text "\\C\\Sv" 2>/dev/null
-# "xclip -selection clipboard"  is to copy $f1 to clipboard without newline.
+# "xclip -selection clipboard"  is to copy $mydate to clipboard without newline.
 # "xvkbd -xsendevent -text "\\C\\Sv" 2>/dev/null"
 #	 reads from the clipboard (in plain text),
 #	 pipes it back into the clipboard,
